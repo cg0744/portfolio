381e150081aabb4160b59e6171eab112914fe831
chrisG074 <1355639@student.rocvantwente.nl>
2026-03-18 01:42:55 +0100

    added

---
+++ PortfolioBackend/Models/ContactRequest.cs
@@ -0,0 +1,9 @@
+namespace PortfolioBackend.Models
+{
+    public class ContactRequest
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Message { get; set; }
+    }
+}
