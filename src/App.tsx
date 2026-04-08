f5227d88646302fb22fa18f35a07ba128cd671e4
christian G <0355639@student.rocvantwente.nl>
2026-04-08 11:37:13 +0200

    added Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import { Analytics } from "@vercel/analytics/react"
 
 // Main layout component
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Experience />
         <Projects />
         <Contact />
+        <Analytics />
       </main>
     </div>
   )
